541b6b949bef9d42b893ee6e22852c9124c5ff3c
chaitali patil <patilchaitu22@gmail.com>
2020-11-23 19:47:07 +0530

    second commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Reusablecard from "./Reusablecard"
 
 const Card = () => {
     return (
@@ -9,55 +10,28 @@ const Card = () => {
           </h2>
           <div className="row">
             <div className="col-4">
-              <div className="card" style={{width:"18rem"}}>
-                <img
-                  src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
-                  </p>
-                  <a href="#" class="btn btn-success">Go somewhere</a>
-                </div>
-              </div>
+              <Reusablecard 
+              imgurl="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              title="Learn Code Online" 
+              text="Learn Code Online"
+              buttontext="Get Plan"
+              />
             </div>
             <div className="col-4">
-              <div className="card" style={{width:"18rem"}}>
-                <img
-                  src="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
-                  </p>
-                  <a href="#" class="btn btn-success">Go somewhere</a>
-                </div>
+            <Reusablecard 
+            imgurl="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              title="GitHub Online" 
+              text="GitHub Online"
+              buttontext="Buy Now"
+              />
               </div>
-            </div>
             <div className="col-4">
-              <div className="card" style={{width:"18rem"}}>
-                <img
-                  src="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
-                  </p>
-                  <a href="#" className="btn btn-success">Go somewhere</a>
-                </div>
-              </div>
+            <Reusablecard 
+            imgurl="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              title="GitBash Online" 
+              text="GitBash Online"
+              buttontext="Enroll Now"
+              />
             </div>
           </div>
         </div>
