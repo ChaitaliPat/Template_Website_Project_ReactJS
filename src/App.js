a66ba2c4f9cc6e8849072b278947ab4447cdf276
chaitali patil <patilchaitu22@gmail.com>
2020-11-23 18:33:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import Appbody from "./Components/Appbody"
+import Card from "./Components/Card"
+import Footer from "./Components/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div>
+        <Navbar />
+        <Hero />
+        <Card />
+        <Appbody />
+        <Footer />
     </div>
-  );
-}
+)
 
 export default App;
